7d1646e96ae8a556f949d5835dac01b90c9a84e7
Philip Warren <philwarren1@gmail.com>
2020-08-16 17:12:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,7 @@
 class Person
-  #your code here
+ attr_accessor :name, :birthday, :hair_color, :location, :bio
+ 
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
 end
